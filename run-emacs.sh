3ea6bae178f70d76643bc2bd8499a091e43a9ec3
jsycdut <jsycdut@gmail.com>
2020-12-30 09:28:24 +0800

    feat: add platform detection in run-emacs.sh

--- run-emacs.sh
+++ run-emacs.sh
@@ -1,4 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-#emacs -q --load init.el init.el
-/Applications/Emacs.app/Contents/MacOS/Emacs -q --load init.el init.el
+PLATFORM=$(uname)
+
+if [[ "$PLATFORM" == "Linux" ]]; then
+  nohup emacs -q --load init.el init.el &
+elif [[ "$PLATFORM" == "Darwin" ]]; then
+  /Applications/Emacs.app/Contents/MacOS/Emacs -q --load init.el init.el
+else
+  echo "Oh~ I don't know what your platform is, try to launch your emacs manually."
+fi
