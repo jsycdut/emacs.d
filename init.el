9322bb50990ad972ccd519a2fcfdf8d643b60b21
jsycdut <jsycdut@gmail.com>
2020-12-30 16:36:50 +0800

    feat(init.el): add useful emacs packages

--- init.el
+++ init.el
@@ -16,7 +16,8 @@
 
 (column-number-mode)
 (global-display-line-numbers-mode t)
-(set-face-attribute 'default nil :height 152)
+(set-face-attribute 'default nil :height 130)
+(setq visible-bell t)
 
 ;;;;;;;;;;;;;;;;;; misc
 (setq browse-url-browser-function 'browse-url-chromium)
@@ -24,12 +25,13 @@
 ;;;;;;;;;;;;;;;;;; Package System
 
 (require 'package)
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
+;;(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+;;                         ("org" . "https://orgmode.org/elpa/")
+;;                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
 ;; mirror for Chinese user
-;;(setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-;;                         ("melpa" . "http://elpa.emacs-china.org/melpa/"))
+(setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+                         ("melpa" . "http://elpa.emacs-china.org/melpa/")))
 (package-initialize)
 
 (unless package-archive-contents
@@ -43,3 +45,93 @@
   (package-install 'use-package))
 (require 'use-package)
 (setq use-package-always-ensure t)
+
+;;;;;;;;;;;;;;;;;; Packages
+(use-package doom-themes
+  :config
+  (load-theme 'doom-solarized-dark t)
+  (setq doom-themes-enable-bold t
+	doom-theme-enable-italic t)
+  (doom-themes-visual-bell-config)
+  (doom-themes-org-config))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package command-log-mode
+  :ensure t
+  :init (command-log-mode 1))
+(use-package all-the-icons)
+
+;; for key sequency completion
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config (setq which-key-idle-delay 0.05))
+
+
+;; enhencement for emacs original functions and command completion
+(use-package swiper
+  :diminish
+  :bind (("C-s" . swiper)
+	 ("C-r" . swiper)
+         ("C-M-j" . 'counsel-switch-buffer)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history))
+  :custom
+  (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
+  :config
+  (counsel-mode 1)
+  (ivy-mode 1))
+
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+;; 
+;; (use-package helpful
+;;   :custom
+;;   (counsel-describe-function-function #'helpful-callable)
+;;   (counsel-describe-variable-function #'helpful-variable)
+;;   :bind
+;;   ([remap describe-function] . counsel-describe-function)
+;;   ([remap describe-command] . helpful-command)
+;;   ([remap describe-variable] . counsel-describe-variable)
+;;   ([remap describe-key] . helpful-key))
+;; 
+
+(use-package evil
+  :init
+  :config
+  (evil-mode 0))
+
+(use-package org)
+(use-package org-superstar
+  :hook (org-mode . org-superstar-mode)
+  :config
+  (setq org-superstar-headline-bullets-list '("☰" "☷" "☯" "☭"))
+  (setq org-indent-mode 1)
+  (setq org-pretty-entities t))
+  
+;; language server protocol(aka lsp) for development
+
+
+;;;;;;;;;;;;;;;;;; key bindings
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
