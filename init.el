87c316d8ebb98260bf939e93e590bff6b79987f6
jsycdut <jsycdut@gmail.com>
2021-01-20 12:52:01 +0800

    feat(init.el): add scala related packages, makes emacs a scala ide

--- init.el
+++ init.el
@@ -317,6 +317,29 @@
 ;;	(setq lsp-java-format-settings-profile "GoogleStyle")
 	(add-hook 'java-mode-hook 'lsp))
 
+;; Scala
+;; Enable scala-mode for highlighting, indentation and motion commands
+(use-package scala-mode
+  :interpreter
+    ("scala" . scala-mode))
+
+;; Enable sbt mode for executing sbt commands
+(use-package sbt-mode
+  :commands sbt-start sbt-command
+  :config
+  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+  ;; allows using SPACE when in the minibuffer
+  (substitute-key-definition
+   'minibuffer-complete-word
+   'self-insert-command
+   minibuffer-local-completion-map)
+   ;; sbt-supershell kills sbt-mode:  https://github.com/hvesalai/emacs-sbt-mode/issues/152
+   (setq sbt:program-options '("-Dsbt.supershell=false"))
+   )
+;; Add metals backend for lsp-mode
+(use-package lsp-metals
+  :config (setq lsp-metals-treeview-show-when-views-received t))
+
 ;; Python
 (use-package lsp-python-ms
   :ensure t
